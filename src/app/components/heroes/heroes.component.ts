58a5416ec400ce1e2542792f6b0c3a4ad27aa083
Cristian Carrino <cristian.carrino@libero.it>
2021-03-16 11:08:16 +0100

    Services

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../models/hero';
+import { HEROES } from 'src/app/data/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: string = 'Windstorm';
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
